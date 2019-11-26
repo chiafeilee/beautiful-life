611d140e3696cb32cc0f0f46837289854ad4bd87
chiafeilee <chiafeilee@gmail.com>
2019-11-26 15:21:54 +0800

    Use sed to modidfy sources.list mirrors

--- dev_tools_install.sh
+++ dev_tools_install.sh
@@ -12,21 +12,7 @@ export PATH
 install_requirements(){
     echo '--> Update apt mirrors to Ali mirrors'
     sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-
-    sudo tee /etc/apt/sources.list <<-'EOF' 
-deb http://mirrors.aliyun.com/ubuntu/ xenial main
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial main
-deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main
-deb http://mirrors.aliyun.com/ubuntu/ xenial universe
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial universe
-deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
-deb http://mirrors.aliyun.com/ubuntu/ xenial-security main
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main
-deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security universe
-EOF
+    sudo sed -i 's#\(//\)\([a-zA-Z]*.ubuntu\)#\1mirrors.aliyun#' /etc/apt/sources.list
 
     echo '--> Initial apt-get update'
     sudo apt-get update -qq >/dev/null
